4bf8375389c736ceeb3959ce4c45e608b9675f67
Richard Conway <richard.conway@telespazio.com>
2019-11-27 17:33:03 +0000

    Add GeneateKey function.

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"io"
 	"log"
 )
+
+// GenerateKey generates a key (random bytes) of the specified size (bits)
+func GenerateKey(key []byte) {
+	sizeBytes := len(key)
+	log.Printf("Generating key of size %v bits => %v bytes\n", (sizeBytes * 8), sizeBytes)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		panic(err)
+	}
+	log.Printf("Key generated => %x\n", key)
+}
 
 // Encrypt encrypt 'plaintext' using 'key' (AES CBC)
 func Encrypt(key []byte, plaintext []byte) []byte {
@@ -18,9 +30,9 @@ func Encrypt(key []byte, plaintext []byte) []byte {
 	// include it at the beginning of the ciphertext.
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
-	// if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-	// 	panic(err)
-	// }
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
 
 	mode := cipher.NewCBCEncrypter(block, iv)
 	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
@@ -31,8 +43,13 @@ func Encrypt(key []byte, plaintext []byte) []byte {
 func main() {
 	log.Println("crypto...")
 
-	key := []byte("1234567890123456")
+	// Generate key
+	const KeySizeBits = 128
+	key := make([]byte, KeySizeBits/8)
+	GenerateKey(key)
+
+	// Encrypt data
 	data := []byte("abcdefghijklmnop")
 	ciphertext := Encrypt(key, data)
-	log.Printf("%x\n", ciphertext)
+	log.Printf("Ciphertext => %x\n", ciphertext)
 }
